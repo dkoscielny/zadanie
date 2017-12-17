ad29e5dbe3e1e031c58123c254d661f3c76458a3
darkos <dar.koscielny@gmail.com>
2017-12-17 13:33:05 +0100

    refactor

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
-function Order(){
+(function orders(){
 
-    var self = this,
-        VAT = 23,
+    var VAT = 23,
         productName,
         costPerItem,
         quantity,
@@ -10,147 +9,145 @@ function Order(){
         midRate_USD,
         midRate_EUR,
         netEUR,
-        netUSD;
+        netUSD,
 
-    self.calculateOrder = function(){
-        productName = $('#newOrder__productName').val();
-        costPerItem = parseFloat( $('#newOrder__costPerItem').val() ).toFixed(2);
-        quantity = $('#newOrder__quantity').val();
-        grossPLN = (quantity* costPerItem ).toFixed(2);
-        netPLN = (quantity* costPerItem* (100- VAT)/100 ).toFixed(2);
+        calculateOrder = function(){
+            productName = $('#newOrder__productName').val();
+            costPerItem = parseFloat( $('#newOrder__costPerItem').val() ).toFixed(2);
+            quantity = $('#newOrder__quantity').val();
+            grossPLN = (quantity* costPerItem ).toFixed(2);
+            netPLN = (quantity* costPerItem* (100- VAT)/100 ).toFixed(2);
 
-        $('#newOrder__summary-modal').modal('show'); 
-        // show message in modal during connecting to the server and calculating
-        $(".summary__counting").show();
-    
-        var getRate_USD = function(){
-            return $.get( "http://api.nbp.pl/api/exchangerates/rates/A/USD/", function(data){
-                midRate_USD = data.rates[0].mid;
-            });
-        },
-    
-            getRate_EUR = function(){
-                return $.get( "http://api.nbp.pl/api/exchangerates/rates/A/EUR/", function(data){
-                    midRate_EUR = data.rates[0].mid;
+            $('#newOrder__summary-modal').modal('show'); 
+            // show message in modal during connecting to the server and calculating
+            $(".summary__counting").show();
+        
+            var getRate_USD = function(){
+                return $.get( "http://api.nbp.pl/api/exchangerates/rates/A/USD/", function(data){
+                    midRate_USD = data.rates[0].mid;
                 });
             },
         
-            putDataIntoModal = function(productName, costPerItem, quantity, grossPLN, netPLN, netEUR, netUSD){
-                var $dataDiv = $('.summary__data'),
-                    i = 0;
-                for(i; i < arguments.length; i++){
-                    $dataDiv.find('div:nth-of-type('+(i+1)+') > span:nth-of-type(2)').text(arguments[i]);
-                }     
-            };
+                getRate_EUR = function(){
+                    return $.get( "http://api.nbp.pl/api/exchangerates/rates/A/EUR/", function(data){
+                        midRate_EUR = data.rates[0].mid;
+                    });
+                },
+            
+                putDataIntoModal = function(productName, costPerItem, quantity, grossPLN, netPLN, netEUR, netUSD){
+                    var $dataDiv = $('.summary__data'),
+                        i = 0;
+                    for(i; i < arguments.length; i++){
+                        $dataDiv.find('div:nth-of-type('+(i+1)+') > span:nth-of-type(2)').text(arguments[i]);
+                    }     
+                };
+            
+            // wait until responses for both requests are recived
+            $.when(
+                getRate_EUR().then(
+                    //on success
+                    function() {
+                        netEUR = ( netPLN/ midRate_EUR ).toFixed(2);
+                    }, 
+                    //on fail
+                    function() {
+                        netEUR = "Nie można pobrać aktualnego kursu.";
+                    }
+                ), 
+                getRate_USD().then(
+                    //on success
+                    function() {
+                        netUSD = ( netPLN/ midRate_USD ).toFixed(2);
+                    }, 
+                    //on fail
+                    function() {
+                        netUSD = "Nie można pobrać aktualnego kursu.";
+                    }
+                )
+            // and then continue
+            ).always(function(){
+                // hide message after all
+                $(".summary__counting").hide();
+                // and show summary
+                $(".summary__data").fadeIn('slow');
         
-        // wait until responses for both requests are recived
-        $.when(
-            getRate_EUR().then(
-                //on success
-                function() {
-                    netEUR = ( netPLN/ midRate_EUR ).toFixed(2);
-                }, 
-                //on fail
-                function() {
-                    netEUR = "Nie można pobrać aktualnego kursu.";
-                }
-            ), 
-            getRate_USD().then(
-                //on success
-                function() {
-                    netUSD = ( netPLN/ midRate_USD ).toFixed(2);
-                }, 
-                //on fail
-                function() {
-                    netUSD = "Nie można pobrać aktualnego kursu.";
-                }
-            )
-        // and then continue
-        ).always(function(){
-            // hide message after all
-            $(".summary__counting").hide();
-            // and show summary
-            $(".summary__data").fadeIn('slow');
-    
-            putDataIntoModal(productName, quantity, costPerItem, grossPLN, netPLN, netEUR, netUSD);
-        });
-    };
-
-    self.addToTheBasket = function(){
-        $tbody = $('.basket__table > tbody');
-        $tr = $('<tr></tr>');
-        
-        var i = 0,
-            rowValues = [productName, quantity, costPerItem, grossPLN, netPLN, netEUR, netUSD];
-
-        for(i; i < rowValues.length; i++){
-            $td = $('<td>' + rowValues[i] + '</td>');
-            $tr.append($td);
-        }
-
-        $tr.append('<td class="basket__deleteItem"><i class="fa fa-trash" aria-hidden="true"></i></td>');
-        $tr.hide();
-
-        $tbody.append($tr);
-        $tr.fadeIn('slow');
-    };
-
-    self.checkIfShowTableFooter = function(){
-        if($('.basket__table tbody').children().length == 0){
-            $('.basket__table tfoot').show('slow');
-        }
-    };
-
-    self.checkIfHideTableFooter = function(){
-        if($('.basket__table tbody').children().length == 1){
-           $('.basket__table tfoot').hide('slow');
-        }
-    };
-
-    self.calculateTotalSum = function(){
-        var i = 4;
-
-        for(i; i <= 7; i++){
-            var sum = 0;
-            $('.basket__table tbody td:nth-of-type('+ i +')').each(function() {
-                sum +=  parseFloat( $(this).text());
+                putDataIntoModal(productName, quantity, costPerItem, grossPLN, netPLN, netEUR, netUSD);
             });
+        },
 
-            $('.basket__table tfoot td:nth-of-type('+ (i-2) +')').text(sum.toFixed(2));
-        }
-    };
+        addToTheBasket = function(){
+            var $tbody = $('.basket__table > tbody'),
+            $tr = $('<tr></tr>');
+            
+            var i = 0,
+                rowValues = [productName, quantity, costPerItem, grossPLN, netPLN, netEUR, netUSD];
 
-}
-var order = new Order();
+            for(i; i < rowValues.length; i++){
+                $td = $('<td>' + rowValues[i] + '</td>');
+                $tr.append($td);
+            }
 
-$( document ).ready(function(){
+            $tr.append('<td class="basket__deleteItem"><i class="fa fa-trash" aria-hidden="true"></i></td>');
+            $tr.hide();
 
-    // form submit action activates calculateOrder function
-    $("#newOrder__form").on('submit', function (e) {
-        e.preventDefault();
-        order.calculateOrder();
-    });
+            $tbody.append($tr);
+            $tr.fadeIn('slow');
+        },
 
-    $('.summary__btn-toBasket').on('click', function(){
-        order.checkIfShowTableFooter(); 
-        order.addToTheBasket();
-        var $form = $("#newOrder__form");
-        $form.trigger('reset');
-        $('#newOrder__summary-modal').modal('hide');
-        order.calculateTotalSum();
-    });
+        checkIfShowTableFooter = function(){
+            if($('.basket__table tbody').children().length == 0){
+                $('.basket__table tfoot').show('slow');
+            }
+        },
 
-    // remove row in the table on deleteItem click
-    $('.basket__table').on('click', '.basket__deleteItem', function(){
-        $row = $(this).parent();
-        $row.hide('slow', function(){ 
-            $row.remove();
-            order.calculateTotalSum();          
+    checkIfHideTableFooter = function(){
+            if($('.basket__table tbody').children().length == 1){
+            $('.basket__table tfoot').hide('slow');
+            }
+        },
+
+        calculateTotalSum = function(){
+            var i = 4;
+
+            for(i; i <= 7; i++){
+                var sum = 0;
+                $('.basket__table tbody td:nth-of-type('+ i +')').each(function() {
+                    sum +=  parseFloat( $(this).text());
+                });
+
+                $('.basket__table tfoot td:nth-of-type('+ (i-2) +')').text(sum.toFixed(2));
+            }
+        };
+
+    $( document ).ready(function(){
+
+        // form submit action activates calculateOrder function
+        $("#newOrder__form").on('submit', function (e) {
+            e.preventDefault();
+            calculateOrder();
         });
-        order.checkIfHideTableFooter(); 
-    });
 
-});
+        $('.summary__btn-toBasket').on('click', function(){
+            checkIfShowTableFooter(); 
+            addToTheBasket();
+            var $form = $("#newOrder__form");
+            $form.trigger('reset');
+            $('#newOrder__summary-modal').modal('hide');
+            calculateTotalSum();
+        });
+
+        // remove row in the table on deleteItem click
+        $('.basket__table').on('click', '.basket__deleteItem', function(){
+            var $row = $(this).parent();
+            $row.hide('slow', function(){ 
+                $row.remove();
+                calculateTotalSum();          
+            });
+            checkIfHideTableFooter(); 
+        });
+
+    });
+}());
     
     
 
